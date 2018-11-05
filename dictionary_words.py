75b1015fc1a098cd8b8087e48a089a08e79ba2c1
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-04 21:25:05 -0800

    generating random words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,25 +1,22 @@
 import random
-from random import shuffle
+import sys
 
-random_words = open("/usr/share/dict/words").read().split()
-shuffle(random_words[:9])
-print(random_words)
+word_list = []
 
+with open("/usr/share/dict/words","r") as f:
+    for line in f:
+        word_list.extend(line.split())
 
+def generate_the_word():
+    random_word = random.choice(word_list)
+    return random_word
 
-
-
-
-
-
-
-
+print(generate_the_word())
 
 
 
 
-# import random
-#
+
 # def generate_the_word(infile):
 #     random_line = random.choice(open(infile).read().split('\n'))
 #     return random_line
