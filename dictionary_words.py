384c5c717cc5932af3ee9d06279d56d5173421e4
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-04 22:37:56 -0800

    Almost done with 2

--- dictionary_words.py
+++ dictionary_words.py
@@ -3,18 +3,34 @@ import sys
 
 word_list = []
 
-with open("random_words.text","r") as f:
+# Open the file and add the words from the file to the word list seperatly
+with open("/usr/share/dict/words","r") as f:
     for line in f.readlines():
         word_list.extend(line.split())
 
+
 def generate_the_word():
+    """ This will pick a random word from the file and return the word """
     random_word = random.choice(word_list)
     return random_word
 
-print(word_list)
-print(generate_the_word())
-print(" ".join([word_list[random.randrange(0, len(word_list))] for i in range(5)]))
+# This will put those range of words together and print it on the console
+print(" ".join([word_list[random.randrange(0, len(word_list))] for i in range(4)]))
 
+# TO do
+    #Need to accept an argument from the user
+    #show the number words words based on how many words the user wants to see
+
+
+
+
+"""
+Links I Looked at:
+https://stackoverflow.com/questions/49144723/python-creating-a-random-sentence-generator-from-dictionary
+https://gist.github.com/bergantine/2390284
+https://stackoverflow.com/questions/3277503/in-python-how-do-i-read-a-file-line-by-line-into-a-list
+https://stackoverflow.com/questions/36706734/reading-words-from-a-file-and-putting-into-list
+"""
 
 
 
