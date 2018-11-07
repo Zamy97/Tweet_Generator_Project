916cd3cbae156fabdeba14007739c9a90e3933ab
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-06 17:11:07 -0800

    working on the last function

--- histogram.py
+++
@@ -1,5 +0,0 @@
-
-with open("source_text.text", "r") as f:
-    for line in f.readlines():
-        for word in line.split():
-            print(word)
---
+++ histogram_dict.py
@@ -0,0 +1,48 @@
+import sys
+import collections
+
+# Finds the number of repeted words in the file
+def find_most_common_words(textfile):
+    """ Return the most common words in a text file. """
+    textfile = open("source_text.text")
+    text = textfile.read().lower()
+    textfile.close()
+    words = collections.Counter(text.split()) # how often each word appears
+    return dict(words.most_common())
+
+word_frequency = find_most_common_words("source_text.text")
+print(word_frequency)
+
+count = {}
+
+# Prints out how many times the unique words are in the file
+def unique_words(find_most_common_words):
+    print ("The number of unique words in the file is: " + str(len(set(w.lower() for w in open("source_text.text").read().split()))))
+    # for w in open("source_text.text").read().split():
+    #     if w in count:
+    #         count[w] += 1
+    #     else:
+    #         count[w] = 1
+    # for word,times in word_frequency.items():
+    #     print(word,times)
+
+unique_words("any")
+
+file_name = input("What file would you like to open? ")
+with open("source_text.text", "r") as f:
+    words = f.read().split()
+
+search_words = input("What words do you want to find? ").split(',')
+search_words = [word.strip().lower() for word in search_words]
+#print(search_words)
+search_counts = dict.fromkeys(search_words, 0)
+
+print ('\n... analyzing ... hold on ...')
+for word in words:
+    word = word.rstrip(",.").lower()
+    if word in search_counts:
+        search_counts[word] += 1
+
+print ('\nFrequency of word usage within', file_name + ":")
+for word in search_words:
+    print("   {:<20s} / {} occurrences".format(word, search_counts[word]))
